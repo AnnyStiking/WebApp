8472e8e70fc79c8bec00cf0bf8bf7742c7ab2907
AnnyStiking <lobova-anya@mail.ru>
2016-08-18 13:40:13 +0300

    Additions

--- WebApp/src/ru/unlimit/MyServlet.java
+++ WebApp/src/ru/unlimit/MyServlet.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.PrintWriter;
+import java.util.Arrays;
 
 
 @WebServlet("/MyServlet")
@@ -18,8 +19,31 @@ public class MyServlet extends HttpServlet {
 		
 		String name = request.getParameter("firstName");
 		String surname = request.getParameter("secondName");
+		//String job = request.getParameter("job");
+		String [] jobs = request.getParameterValues("job");
+		String gender = request.getParameter("gender");
+			
+			if (gender == null)
+			{
+				gender = "-";
+			}
+			
+		String age18 = request.getParameter("age18");
+		
+		if (age18 == null)
+		{
+			age18 = "-";
+		}
+		
 		System.out.println("Имя: " + name);
 		System.out.println("Фамилия: " + surname);
+		//System.out.println("Профессия: " + job);
+		System.out.println("Количество ролей: " + jobs.length);
+		for (String job : jobs) {
+			System.out.print(job + ", ");
+		}
+		System.out.println("Пол: " + gender);
+		System.out.println("Старше 18: " + age18);
 		
 	//	response.setContentType("text/html");
 	//	response.setCharacterEncoding("UTF-8");
@@ -29,6 +53,10 @@ public class MyServlet extends HttpServlet {
 		out.println("<h3>Профиль сотрудника</h3>");
 		out.println("Имя: " + name + "<br>");
 		out.println("Фамилия: " + surname + "<br>");
+		//out.println("Профессия: " + job + "<br>");
+		out.println("Профессия: " + Arrays.deepToString(jobs) + "<br>");
+		out.println("Пол: " + gender + "<br>");
+		out.println("Старше 18: " + age18 + "<br>");
 		//out.println("<h3>Hello from MyServlet.GET " + name + " " + surname + "</h3>");
 		out.close();
 		
