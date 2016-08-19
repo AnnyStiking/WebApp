0cd6cc76fa9bd7785b190add6ccec6de589df2e3
AnnyStiking <lobova-anya@mail.ru>
2016-08-19 14:05:36 +0300

    Life cycle of Request, Session, Context

--- WebApp/src/ru/unlimit/ParametersServlet.java
+++ WebApp/src/ru/unlimit/ParametersServlet.java
@@ -11,10 +11,18 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-@WebServlet("/ParametersServlet")
+@WebServlet(urlPatterns = "/ParametersServlet", loadOnStartup = 0)
 public class ParametersServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+	
+	@Override
+	public void init() throws ServletException {
+		super.init();
+		System.out.println("Initialization of servlet");
+	}
+
+
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		String user = request.getParameter("user");
@@ -24,7 +32,7 @@ public class ParametersServlet extends HttpServlet {
 			session.setAttribute("user", user);
 			context.setAttribute("user", user);
 		}
-		//session.setMaxInactiveInterval(5);
+	//	session.setMaxInactiveInterval(5);
 		response.setContentType("text/html; charset = utf-8");
 		PrintWriter out = response.getWriter();
 		out.println("Request parameter: " + user + "<br>");
